227d4bc5ca1825f938f8b5ac0d604be431babc95
CalciumCrusader <44983783+MrSkeletall@users.noreply.github.com>
2022-04-04 14:48:29 -0700

    initial commit

---
+++ multifile-kaboomjs-master/main.js
@@ -0,0 +1,12 @@
+import {k} from "./kaboom.js"
+
+// add a piece of text at position (120, 80)
+k.loadSprite("ground.png", "./sprites/ground.png");
+
+k.add([
+    k.scale(2),
+    k.text("hello world"),
+    k.pos(50, 80),
+]);
+
+k.onClick(() => k.go("start"))
---
+++ multifile-kaboomjs-master/player.js
@@ -0,0 +1,7 @@
+import {k} from "./kaboom.js"
+
+
+const moveSpeed = 100;
+const jumpForce = 100;
+
+let player = k.add

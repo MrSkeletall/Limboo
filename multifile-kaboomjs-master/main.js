7a0215215d8d9c2129a1bf1aaa4821c378376039
CalciumCrusader <44983783+MrSkeletall@users.noreply.github.com>
2022-04-04 19:53:29 -0700

    added basic movement and test scene

--- multifile-kaboomjs-master/main.js
+++ multifile-kaboomjs-master/main.js
@@ -1,12 +1,68 @@
 import {k} from "./kaboom.js"
 
-// add a piece of text at position (120, 80)
-k.loadSprite("ground.png", "./sprites/ground.png");
+k.loadSprite("tempHuman", "../sprites/tempPlayerHuman.png")
+k.loadSprite("tempGhost", "../sprites/tempPlayerGhost.png")
 
-k.add([
-    k.scale(2),
-    k.text("hello world"),
-    k.pos(50, 80),
+let currentSprite = "tempHuman"
+
+const playerSpeed = 500;
+
+let player = k.add([
+    k.sprite(currentSprite),
+    //k.color(255, 0, 0),
+    k.pos(100, 100),
+    k.area(),
+    k.body(),
+    k.state("human", ["human", "ghost"])
+
 ]);
 
-k.onClick(() => k.go("start"))
+//player movement
+k.onKeyDown("d", () => {
+    player.move(playerSpeed, 0);
+})
+
+k.onKeyDown("a", () => {
+    player.move(-playerSpeed, 0);
+})
+
+k.onKeyPress("space", () => {
+    player.jump();
+})
+
+k.onKeyPress("j", () => {
+    if(player.state == "human"){
+        player.enterState("ghost");
+    }
+    if(player.state == "ghost"){
+       player.enterState("human");
+    }
+})
+
+//state machine
+player.onStateEnter("ghost", () => {
+    //for some god forsaken reason, ya cant change the sprite...
+    player.sprite = player.sprite("tempGhost");
+})
+
+
+//platforms for testing, 
+k.add([
+    k.rect(k.width(), 48),
+    k.pos(0, k.height() - 48),
+    k.outline(4),
+    k.area(),
+    k.solid(),
+    k.color(200, 100, 255),
+])
+
+k.add([
+    k.rect(48, k.height()),
+    k.pos(300, 0),
+    k.outline(4),
+    k.area(),
+    k.solid(),
+    k.color(127, 200, 255),
+    "ghostBlock",
+
+])
